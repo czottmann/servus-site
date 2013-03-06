34206356030732f85954af02c52a17f1d49527dc
Carlo Zottmann <carlo@municode.de>
2013-03-06 23:47:02 +0100

    [CHG] Split up _top.js in several smaller files.

--- src/controller.rb
+++ src/controller.rb
@@ -7,8 +7,14 @@ layout "layout.html.haml"
 ignore /\/_.*\.(scss|js)/
 ignore /\/js\/foundation.*/
 
+# Concatenate all relevant JS files into `js/all.js`
 before 'js/all.js' do |f|
-  instead %w( foundation/foundation foundation/foundation.topbar _top ).map { |name|
-    render("js/#{name}.js")
-  }.join("\n\n")
+  js_files = %w( foundation/foundation.js foundation/foundation.topbar.js _top.js ) \
+    + Dir.glob("_page*")
+
+  js_output = js_files.map do |name|
+    render(name)
+  end
+
+  instead js_output.join("\n\n")
 end
