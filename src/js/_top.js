c03c7a86701ba0e95b1aeb0a4e4cebea50ec6316
Carlo Zottmann <carlo@municode.de>
2013-05-05 14:10:57 +0200

    [CHG] Moved tracker code to top.

--- src/js/_top.js
+++ src/js/_top.js
@@ -16,41 +16,6 @@ else if ( document.location.host !== "servus.io" && document.location.hostname !
 }
 
 
-// GoSquared
-var GoSquared = { acct: "GSN-208108-B" };
-
-// Google Analytics
-var _gaq = _gaq || [];
-_gaq.push(["_setAccount", "UA-3142618-11"]);
-_gaq.push(["_setDomainName", "servus.io"]);
-_gaq.push(["_setAllowLinker", true]);
-_gaq.push(["_trackPageview"]);
-
-(function(isLocalhost) {
-  if (isLocalhost) {
-    return;
-  }
-
-  var d = document,
-    gs = d.createElement("script"),
-    ga = d.createElement("script"),
-    firstScript = d.getElementsByTagName("script")[0];
-
-  // GoSquared
-  window._gstc_lt = +new Date();
-  gs.type = "text/javascript";
-  gs.src = "//d1l6p2sc9645hc.cloudfront.net/tracker.js";
-  gs.async = true;
-  firstScript.parentNode.insertBefore(gs, firstScript);
-
-  // Google Analytics
-  ga.type = "text/javascript";
-  ga.src = "https://ssl.google-analytics.com/ga.js";
-  ga.async = true;
-  firstScript.parentNode.insertBefore(ga, firstScript);
-}( document.location.hostname === "localhost" ) );
-
-
 // Initialize Foundation.
 $(document).foundation();
 
