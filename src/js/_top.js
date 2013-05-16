e0b751a6c91540a86a70dc60f585dd4cafe5accf
Carlo Zottmann <carlo@municode.de>
2013-05-17 00:19:26 +0200

    [NEW] Added new PWYW buy page, it's in working order.

--- src/js/_top.js
+++ src/js/_top.js
@@ -75,3 +75,114 @@ $(document).ready( function() {
     );
   });
 });
+
+
+// Pay-What-You-Want functionality
+$(document).ready( function() {
+  if ( !$("#pwyw").length ) {
+    return;
+  }
+
+  var rangeLow = 2,
+    rangeHigh = 20,
+    suggestedPrice = 5;
+
+  function getComputedPrices(rawValue) {
+    return {
+      eur: +rawValue,
+      usd: +( rawValue * 1.3 ).toPrecision(3)
+    };
+  }
+
+  function getFormattedComputedPrices(rawValue) {
+    var prices = getComputedPrices(rawValue);
+
+    return {
+      eur: prices.eur.toFixed(2).replace( /\.00$/, "" ),
+      usd: prices.usd.toFixed(2).replace( /\.00$/, "" )
+    };
+  }
+
+  function updateCurrentPriceDisplay(rawValue) {
+    var prices = getFormattedComputedPrices(rawValue);
+
+    $("#pwyw .js-selected-price").text(
+      [ "€", prices.eur,
+        " / ",
+        "$", prices.usd
+      ].join("")
+    );
+  }
+
+  function renderPriceRangeDisplay() {
+    var priceLow = getFormattedComputedPrices(rangeLow),
+      priceHigh = getFormattedComputedPrices(rangeHigh);
+
+    $("#pwyw")
+      .find(".js-range-low").html(
+        [ "€", priceLow.eur,
+          "<br>",
+          "$", priceLow.usd
+        ].join("")
+      )
+      .end()
+      .find(".js-range-high").html(
+        [ "€", priceHigh.eur,
+          "<br>",
+          "$", priceHigh.usd
+        ].join("")
+      );
+  }
+
+  function updateStoreLink(rawValue) {
+    var prices = getFormattedComputedPrices(rawValue),
+      link = document.createElement('a'),
+      $elem = $("#pwyw a.button-buy");
+
+    link.href = $elem.attr("href");
+    link.search = [
+      "?tags=total_eur=",
+      prices.eur * 100,
+      ",total_usd=",
+      prices.usd * 100
+    ].join("");
+
+    $("#pwyw a.button-buy").attr({
+      href: link.href
+    });
+  }
+
+  $("#pwyw-slider").noUiSlider({
+    handles: 1,
+    range: [ rangeLow, rangeHigh ],
+    start: suggestedPrice,
+    step: 0.5,
+    slide: function() {
+      var price = +$(this).val();
+
+      updateCurrentPriceDisplay(price);
+      updateStoreLink(price);
+    }
+  });
+
+  renderPriceRangeDisplay();
+  updateCurrentPriceDisplay(suggestedPrice);
+  updateStoreLink(suggestedPrice);
+
+  // Toggle "read more" section visibility
+  $("#read-more")
+    .data("original-height", $("#read-more").height() )
+    .height(0)
+    .css( "opacity", 0 );
+
+  $(".js-toggle-read-more").one( "click", function() {
+    var $section = $("#read-more");
+
+    $section.animate({
+      height: !$section.height() ? $section.data("original-height") : 0,
+      opacity: !$section.height() ? 1 : 0
+    }, 300 );
+
+    $(".js-toggle-read-more").remove();
+  });
+});
