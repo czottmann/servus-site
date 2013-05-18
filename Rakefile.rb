15d5e3d4ff33edf39ab6b8262b1ea99a0c510f63
Carlo Zottmann <carlo@municode.de>
2013-05-18 12:38:45 +0200

    [CHG] Renamed file.

---
+++ Rakefile.rb
@@ -0,0 +1,37 @@
+require "bundler/setup"
+Bundler.require
+
+
+namespace :dev do
+
+  desc "Generate the site in ./build/ from the files in ./src/."
+  task :generate do
+    system("cd src; stasis -p ../build/; cd ..")
+  end
+
+
+  desc "Auto-generation mode, open localhost:3000."
+  task :auto_generate do
+    puts "", "Development server running: http://localhost:3000"
+    # Stasis::DevMode doesn't work as expected, dunno why.  Too lazy to dig
+    # deeper right now.
+    system("cd src; stasis -d 3000 -p ../build/; cd ..")
+  end
+
+end
+
+
+namespace :git do
+
+  desc "Commit regenerated build folder."
+  task :commit_build do
+    system("git commit -m '[NEW] Re-generated build folder.' build/")
+  end
+
+end
+
+
+desc "Deploy to Heroku (by pushing to all remotes)."
+task :deploy do
+  system("git push all master")
+end
